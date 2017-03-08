3b92ca5131f19179ed40e1ac2e7b9bd7b1ddef8b
Thomas Conklin <tconkin63@gmail.com>
2017-03-08 12:24:48 -0800

    Debug on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.consider_all_requests_local = true
 
   HOST_NAME = 'bairdconklinmath.herokuapp.com'
 end
